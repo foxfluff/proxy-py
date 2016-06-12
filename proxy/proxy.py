151018fa7fdf990eaa61526a60ebe43c21e1090d
foxfluff <foxfluff@users.noreply.github.com>
2016-06-12 16:37:56 -0400

    Initial Commit - basic layout for structure

---
+++ proxy/proxy.py
@@ -0,0 +1,18 @@
+import sys
+import socket
+import threading
+
+class Connection:
+
+    def __init__(self):
+        pass
+
+    def connect_in(self):
+        pass
+
+    def connect_out(self):
+        pass
+
+    def main(self):
+        pass
+
