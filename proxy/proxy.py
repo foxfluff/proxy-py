1ab2190e888f9c0ef95ecfec2404614a81e4e803
foxfluff <foxfluff@users.noreply.github.com>
2016-06-12 17:21:20 -0400

    Minor tweaks to argparse, buffer handling, setting up connection loop

--- proxy/proxy.py
+++ proxy/proxy.py
@@ -5,8 +5,12 @@ import argparse
 
 class Connection:
 
-    def __init__(self):
-        pass
+    def __init__(self, ):
+        #buffers
+        self._machine_a_in = ''
+        self._machine_a_out = ''
+        self._machine_b_in = ''
+        self._machine_b_out = ''
 
     def connect_in(self):
         pass
@@ -28,9 +32,20 @@ class Connection:
 
 arg_parser = argparse.ArgumentParser(description = "Simple 1:1 proxy - Example usage: proxy.py -p 8080 -r 192.168.0.1 -d 80")
 arg_parser.add_argument('-l', '--local-addr', default = '0.0.0.0', dest = 'local_address', help = 'The local address to bind to (default: 0.0.0.0)')
-arg_parser.add_argument('-p', '--local-port', dest = 'local_port', help = 'The local TCP port to bind to')
-arg_parser.add_argument('-r', '--remote-addr', dest = 'remote_address', help = 'The remote address to connect to')
-arg_parser.add_argument('-d', '--remote-port', dest = 'remote_port', help = 'The remote port to connect to')
+arg_parser.add_argument('-p', '--local-port', required = True, dest = 'local_port', help = 'The local TCP port to bind to')
+arg_parser.add_argument('-r', '--remote-addr', required = True, dest = 'remote_address', help = 'The remote address to connect to')
+arg_parser.add_argument('-d', '--remote-port', required = True, dest = 'remote_port', help = 'The remote port to connect to')
 
 args = arg_parser.parse_args()
 #apparently parse_args() will just close the program here if the user asks for help (-h)
+
+#game/connection loop
+
+while True:
+    try:
+        #listen for stuff
+        #when something connects, spawn a thread and try to establish the outgoing connection
+        #profit
+    except KeyboardInterrupt:
+        #clean up please
+        break # <- this is not clean >:V
