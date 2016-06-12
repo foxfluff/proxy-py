d456ff5db98625dbe38cd90c0f17c09550cb91f7
foxfluff <foxfluff@users.noreply.github.com>
2016-06-12 17:11:52 -0400

    Added argparse for logical command parsing.

--- proxy/proxy.py
+++ proxy/proxy.py
@@ -1,6 +1,7 @@
-import sys
+#import sys #maybe not necessary if argparse already handles all of this
 import socket
 import threading
+import argparse
 
 class Connection:
 
@@ -13,6 +14,23 @@ class Connection:
     def connect_out(self):
         pass
 
+    def udp_in(self):
+        raise NotImplementedError
+
+    def udp_out(self):
+        raise NotImplementedError
+
     def main(self):
         pass
 
+# interpret args passed to program
+# localaddr, localport, remoteaddr, remoteport
+
+arg_parser = argparse.ArgumentParser(description = "Simple 1:1 proxy - Example usage: proxy.py -p 8080 -r 192.168.0.1 -d 80")
+arg_parser.add_argument('-l', '--local-addr', default = '0.0.0.0', dest = 'local_address', help = 'The local address to bind to (default: 0.0.0.0)')
+arg_parser.add_argument('-p', '--local-port', dest = 'local_port', help = 'The local TCP port to bind to')
+arg_parser.add_argument('-r', '--remote-addr', dest = 'remote_address', help = 'The remote address to connect to')
+arg_parser.add_argument('-d', '--remote-port', dest = 'remote_port', help = 'The remote port to connect to')
+
+args = arg_parser.parse_args()
+#apparently parse_args() will just close the program here if the user asks for help (-h)
